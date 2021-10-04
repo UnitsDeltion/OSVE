18a50b822a393b55295e1c160c410935dcd4a860
martijnschuman <31829786+martijnschuman@users.noreply.github.com>
2021-10-04 17:51:21 +0200

    Migrations

---
+++ database/migrations/2021_10_04_154818_create_opleidingen_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOpleidingenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('opleidingen', function (Blueprint $table) {
+            $table->integer('crebo_nr')->index('crebo_nr');
+            $table->string('opleiding_naam');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('opleidingen');
+    }
+}
