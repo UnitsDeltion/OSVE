5c5b09bc588f8747f19f9efcbf84782565f7678f
Pascal Palmbergen <97071583@st.deltion.nl>
2021-10-05 12:34:33 +0200

    css martijn noob

--- resources/views/p3.blade.php
+++ resources/views/p3.blade.php
@@ -1,13 +1,15 @@
 <x-app-layout>
-    <div class="card">
-    <div class="card-header">
-        Examens
-    </div>
-    <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
+    <div class="ml-250">
+        <div class="card  col-sm-4">
+            <div class="card-header">
+                Examens
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
     </div>
 
 
