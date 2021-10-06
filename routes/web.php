4da12212545c56f2795a0455ca8da5971ae8b95a
martijnschuman <martijnschuman@hotmail.com>
2021-10-06 14:02:07 +0200

    General

--- routes/web.php
+++ routes/web.php
@@ -16,10 +16,6 @@ use App\Http\Controllers\ExamenBeheerController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
