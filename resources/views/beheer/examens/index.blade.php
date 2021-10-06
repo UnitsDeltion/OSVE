0c36fe9a4a0eee0ea84a85fc442f30e5df0b48fa
Spatzy <bas.plat@outlook.com>
2021-10-06 11:09:14 +0200

    beheer index

---
+++ resources/views/beheer/examens/index.blade.php
@@ -0,0 +1,38 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2>
+            @section('title', 'Alle examens')
+            @yield('title')
+        </h2>
+    </x-slot>
+
+    @livewire('includes.content.top.content-normal-top') 
+
+                                <table class="w-full dd-primary-global fc-white fz-14 br-5">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col" class="px-6 py-3 text-left font-medium uppercase tracking-wider">
+                                                <strong>Examens</strong>
+                                            </th>
+                                            <th scope="col" class="px-6 py-3 text-left font-medium uppercase tracking-wider">
+                                                <strong>Datums</strong>
+                                            </th>
+                                            <th scope="col" class="px-6 py-3 text-left font-medium uppercase tracking-wider">
+                                                <strong>Beschikbare plekken</strong>
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="bg-white divide-y divide-gray-200">
+                                        @foreach ($exams as $exam)
+                                        <tr>
+                                        <td class="px-6 whitespace-nowrap text-sm text-gray-900">
+                                                    {{ $exam->name }}
+                                                </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+
+    @livewire('includes.content.bottom.content-bottom') 
+
+</x-app-layout>
