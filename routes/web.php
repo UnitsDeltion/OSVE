1ada97a4dae1ebc65f200170ed7c7f0fbba6e2b0
martijnschuman <martijnschuman@hotmail.com>
2021-10-06 13:37:08 +0200

    Controller move

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\ExamenBeheerController;
+use App\Http\Controllers\Beheer\ExamenBeheerController;
 
 /*
 |--------------------------------------------------------------------------
