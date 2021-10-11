f6e20d284be3c2aa646c25494dfd73c3c998d6ef
martijnschuman <martijnschuman@hotmail.com>
2021-10-11 15:00:07 +0200

    css

--- resources/views/beheer/opleidingen/index.blade.php
+++ resources/views/beheer/opleidingen/index.blade.php
@@ -8,35 +8,35 @@
 
     @livewire('includes.content.top.content-normal-top') 
 
-    
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Crebo nummer</th>
-                                    <th>Opleiding</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            @foreach($opleidingen as $opleiding)
-                                <tr class="">
-                                    <td>{{$opleiding['crebo_nr']}}</td>
-                                    <td>{{$opleiding['opleiding_naam']}}</td>
-                                    <td class="px-6 whitespace-nowrap text-sm font-medium d-flex flex-row-reverse">
-                                    <a href="#" class="mb-2 mr-2 a-clear">
-                                        <x-jet-button class="button">
-                                            <i class="fas fa-edit fc-secondary fc-h-white"></i>
-                                        </x-jet-button>
-                                    </a>
-                                    <a href="#" class="mb-2 mr-2 a-clear">
-                                        <x-jet-button class="button">
-                                            <i class="fas fa-trash fc-secondary fc-h-white"></i>
-                                        </x-jet-button>
-                                    </a>
-                                </tr>
-                            @endforeach
-                        </table>
-
-                    
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Crebo nummer</th>
+                    <th>Opleiding</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($opleidingen as $opleiding)
+                    <tr class="">
+                        <td>{{$opleiding['crebo_nr']}}</td>
+                        <td>{{$opleiding['opleiding_naam']}}</td>
+                        <td class="px-6 whitespace-nowrap text-sm font-medium">
+                            <a href="#" class="mb-2 mr-2 a-clear">
+                                <x-jet-button class="button">
+                                    <i class="fas fa-edit"></i>
+                                </x-jet-button>
+                            </a>
+                            <a href="#" class="mb-2 mr-2 a-clear">
+                                <x-jet-button class="button">
+                                    <i class="fas fa-trash"></i>
+                                </x-jet-button>
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>            
 
     @livewire('includes.content.bottom.content-bottom') 
 
