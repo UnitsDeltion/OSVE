ec21bc8e736728385631380d831fcd0455dd5279
97032722 <jesse@hardwarehulp.nl>
2021-10-12 14:34:04 +0200

    Migration fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
             $table->string('voornaam');
             $table->string('achternaam');
             $table->string('telefoonnummer');
