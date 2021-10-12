14c7ddc9ae8fa6de72824ba46509c8bf6e26b523
martijnschuman <31829786+martijnschuman@users.noreply.github.com>
2021-10-12 20:12:58 +0200

    fixes

--- database/seeders/ExamenTableSeeder.php
+++ database/seeders/ExamenTableSeeder.php
@@ -20,74 +20,74 @@ class ExamenTableSeeder extends Seeder
                 'examen'            =>      'B1 K1 Hospitality',
                 'vak'               =>      'Hospitality',
                 'plaatsen'          =>      '30',
-                'geplande_docent'   =>      '97071583@st.deltion.nl',
+                'geplande_docenten'   =>      '97071583@st.deltion.nl',
             ],
             [
                 'crebo_nr'          =>      25351,
                 'examen'            =>      'P1 K1 TREX',
                 'vak'               =>      'TREX',
                 'plaatsen'          =>      '30',
-                'geplande_docent'   =>      '97071583@st.deltion.nl',
+                'geplande_docenten'   =>      '97071583@st.deltion.nl',
             ],
             [
                 'crebo_nr'          =>      25351,
                 'examen'            =>      'Schrijven',
                 'vak'               =>      'Duits',
                 'plaatsen'          =>      '30',
-                'geplande_docent'   =>      '97071583@st.deltion.nl',
+                'geplande_docenten'   =>      '97071583@st.deltion.nl',
             ],
             [
                 'crebo_nr'          =>      25351,
                 'examen'            =>      'Spreken',
                 'vak'               =>      'Duits',
                 'plaatsen'          =>      '30',
-                'geplande_docent'   =>      '97071583@st.deltion.nl',
+                'geplande_docenten'   =>      '97071583@st.deltion.nl',
             ],
             [
                 'crebo_nr'          =>      25351,
                 'examen'            =>      'Schrijven B1',
                 'vak'               =>      'Engels Generiek',
                 'plaatsen'          =>      '30',
-                'geplande_docent'   =>      '97071583@st.deltion.nl',
+                'geplande_docenten'   =>      '97071583@st.deltion.nl',
             ],
             [
                 'crebo_nr'          =>      25351,
                 'examen'            =>      'Spreken B1',
                 'vak'               =>      'Engels Generiek',
                 'plaatsen'          =>      '30',
-                'geplande_docent'   =>      '97071583@st.deltion.nl',
+                'geplande_docenten'   =>      '97071583@st.deltion.nl',
             ],
             [
                 'crebo_nr'          =>      25351,
                 'examen'            =>      'Gesprekken voeren B1',
                 'vak'               =>      'Engels Generiek',
                 'plaatsen'          =>      '30',
-                'geplande_docent'   =>      '97071583@st.deltion.nl',
+                'geplande_docenten'   =>      '97071583@st.deltion.nl',
             ],
             [
                 'crebo_nr'          =>      25351,
                 'examen'            =>      'Lezen A2',
                 'vak'               =>      'Engels Generiek',
                 'plaatsen'          =>      '30',
-                'geplande_docent'   =>      '97071583@st.deltion.nl',
+                'geplande_docenten'   =>      '97071583@st.deltion.nl',
             ],
             [
                 'crebo_nr'          =>      25351,
                 'examen'            =>      'B1 K2 Back Office',
                 'vak'               =>      'Back Office',
                 'plaatsen'          =>      '30',
-                'geplande_docent'   =>      '97071583@st.deltion.nl',
+                'geplande_docenten'   =>      '97071583@st.deltion.nl',
             ],
             [
                 'crebo_nr'          =>      25351,
                 'examen'            =>      'Luisteren',
                 'vak'               =>      'Duits',
                 'plaatsen'          =>      '30',
-                'geplande_docent'   =>      '97071583@st.deltion.nl',
+                'geplande_docenten'   =>      '97071583@st.deltion.nl',
             ],
 
         ];
 
-        Opleidingen::insert($opleidingen);
+        Examen::insert($examens);
     }
 }
--- resources/views/p3.blade.php
+++ resources/views/p3.blade.php
@@ -13,42 +13,41 @@
                     <div class="mb-20">
                         <div class="container mb-10 ">
                             <div class="row">
-                            @error('examen')<div class="fc-red text-sm mb-2 text-center">{{ $message }}</div>@enderror
-                                    <?php    
-                                        $examenVak = "";
-                                    ?>
-                                        @foreach($examens as $examen)
-                                            <?php
-                                                if($examen->vak != $examenVak )
+                                @error('examen')<div class="fc-red text-sm mb-2 text-center">{{ $message }}</div>@enderror
+                                <?php    
+                                    $examenVak = "";
+                                ?>
+                                    @foreach($examens as $examen)
+                                        <?php
+                                            if($examen->vak != $examenVak )
+                                            {
+                                                if ($examenVak != "")
                                                 {
-                                                    if ($examenVak != "")
-                                                    {
-                                                        echo "</div>";
-                                                    }
-                                                    echo "<div class=\"col-xs-5 mr-10 ml-10 mt-20 p-3 shadow\">";
-                                                    echo "<h3>" . $examen->vak . "</h3>";
+                                                    echo "</div>";
                                                 }
-                                                
-                                                $examenVak = $examen->vak;                                    
-                                            ?>
-                                            <div class="row">
-                                                <div class="col-xs-8">
-                                                    {{ $examen->examen }}
-                                                </div>
-                                                
-                                                <div class="col-xs-2">
-                                                    <i class="far fa-user fc-secondary"></i> {{ $examen->plaatsen }}
-                                                </div>
-                                                <div class="col-xs-2">
-                                                    <input type="radio" name="examen" value="{{ $examen->examen }}">
-                                                </div>
+                                                echo "<div class=\"col-xs-5 mr-10 ml-10 mt-20 p-3 shadow\">";
+                                                echo "<h3>" . $examen->vak . "</h3>";
+                                            }
+                                            
+                                            $examenVak = $examen->vak;                                    
+                                        ?>
+                                        <div class="row">
+                                            <div class="col-xs-8">
+                                                {{ $examen->examen }}
                                             </div>
-                                            @endforeach
+                                            
+                                            <div class="col-xs-2">
+                                                <i class="far fa-user fc-secondary"></i> {{ $examen->plaatsen }}
+                                            </div>
+                                            <div class="col-xs-2">
+                                                <input type="radio" name="examen" value="{{ $examen->examen }}">
+                                            </div>
+                                        </div>
+                                        @endforeach
                                     </div>
                                 </div>  
                             </div>
-                        </div>
-                    </div>
+                        
 
                     <a href="{{ route('p2') }}" class="fc-h-white a-clear float-left mb-2 button inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition button float-right">
                             <i class="fas fa-backward mr-2"></i> Terug
