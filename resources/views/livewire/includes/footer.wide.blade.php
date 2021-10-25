8c562924314251990e1ddf29ed6611de0a3ad837
martijnschuman <martijnschuman@hotmail.com>
2021-10-25 14:45:08 +0200

    Create footer.wide.blade.php

---
+++ resources/views/livewire/includes/footer.wide.blade.php
@@ -0,0 +1,55 @@
+<div>
+    <div class="container">
+        <footer class="footer">
+            <div class="wrap">
+                <div class="row">
+                    <div class="col-xl-12">
+                        <div class="footer__contact">
+                            <div class="row align-items-center-xs">
+                                <div class="col-md-3"></div>
+                                <div class="col-sm-2"></div>
+                                <div class="col-xs-12 col-sm-8 col-md-6 col-xl-3">
+                                    <div class="footer__contact-logo"><img src="{{ asset('images/logos/logo-invert.svg') }}" alt="Deltion"></div>
+                                </div>
+                                <div class="col-xs-6 col-sm-6 col-md-6 col-xl-3">
+                                    <div class="footer__contact-item">
+                                        <p class="footer__contact-label mt-4 mb-10-n">Telefonisch contact</p>
+                                        <p class="footer__contact-value"><a href="tel:+038 850 30 00">038 850 30 00</a></p>
+                                    </div>
+                                </div>
+                                <div class="col-xs-6 col-sm-6 col-md-6 col-xl-3">
+                                    <div class="footer__contact-item">
+                                        <p class="footer__contact-label mt-4 mb-10-n">Mail contact</p>
+                                        <p class="footer__contact-value"><a href="mailto:ssc@deltion.nl">ssc@deltion.nl</a></p>
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12 col-xl-3">
+                                    <ul class="footer__social mb-0 pl-0">
+                                        <li class="footer__social-item"><a href="https://www.twitter.com/deltion" target="_blank"><i class="fz-23 fc-white fab fa-twitter"></i></a></li>
+                                        <li class="footer__social-item"><a href="https://www.youtube.com/channel/UCLVqRDE4Y6hNKLbpKAi15YQ" target="_blank"><i class="fz-23 fc-white fab fa-youtube"></i></a></li>
+                                        <li class="footer__social-item"><a href="https://www.instagram.com/deltion_college/" target="_blank"><i class="fz-23 fc-white fab fa-instagram"></i></a></li>
+                                        <li class="footer__social-item"><a href="https://www.facebook.com/deltioncollege" target="_blank"><i class="fz-23 fc-white fab fa-facebook-f"></i></a></li>
+                                        <li class="footer__social-item"><a href="https://nl.linkedin.com/school/deltion-college/" target="_blank"><i class="fz-23 fc-white fab fa-linkedin-in"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12">
+                        <div class="footer__legal">
+                            <nav class="footer__legal-navigation">
+                                <ul class="footer__legal-list">
+                                    <li class="footer__legal-item"><a class="footer__legal-link" href="https://www.deltion.nl/privacy">Privacyverklaring</a></li>
+                                    <li class="footer__legal-item"><a class="footer__legal-link" href="https://www.deltion.nl/disclaimer">Disclaimer</a></li>
+                                    <li class="footer__legal-item"><a class="footer__legal-link" href="https://www.deltion.nl/responsible-disclosure">Responsible disclosure</a>
+                                    <li class="footer__legal-item"><a class="footer__legal-link" href="https://www.deltion.nl/cert">CERT</a>
+                                    <li class="footer__legal-item"><a class="footer__legal-link" href="https://www.deltion.nl/cookieverklaring">Cookieverklaring</a>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
