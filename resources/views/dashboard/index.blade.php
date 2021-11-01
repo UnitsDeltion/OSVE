4a601729221d8ef421710c2d312ca320d75426e6
martijnschuman <martijnschuman@hotmail.com>
2021-11-01 08:50:11 +0100

    Vertaling, bootstrap

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -6,23 +6,6 @@
     </x-slot>
 
     @livewire('includes.content.top.content-wide-top') 
-    
-        <div class="card-header">
-            Ingeplande Examens
-        </div>
-
-        <form action="/dashboard/klas" method="post" >
-            @csrf
-            <tr>
-                <td>klassen</td>
-                <td> <input type="text" id="klas" name="klas"></td>
-            </tr>
-            <div class="text-center mb-2">
-                <x-jet-button class="dd-primary">
-                    {{ __('Filter') }}
-                </x-jet-button>
-            </div>
-        </form>
 
         <table class="table table-bordered" style="margin: 10px 0 10px 0;" id="form">
             <thead>
@@ -55,7 +38,36 @@
 </x-app-layout>
 
 <script>
-    $(document).ready( function () {
-    $('#form').DataTable();
-} );
+// $(document).ready( function () {
+//     $('#form').DataTable();
+// } );
+
+$(document).ready(function() {
+    $('#form').DataTable( {
+        "language": {
+            "decimal":        "",
+            "emptyTable":     "Geen examens ingepland",
+            "info":           "Pagina _PAGE_ van _PAGES_ weergeven",
+            "infoEmpty":      "0 tot 0 van 0 items weergeven",
+            "infoFiltered":   "(gefilterd uit _MAX_ totale records)",
+            "infoPostFix":    "",
+            "thousands":      ",",
+            "lengthMenu":     "Toon _MENU_ records per pagina",
+            "loadingRecords": "Laden...",
+            "processing":     "Verwerken...",
+            "search":         "Zoeken:",
+            "zeroRecords":    "Geen ingeplande examens gevonden",
+            "paginate": {
+                "first":      "Eerste",
+                "last":       "Laatste",
+                "next":       "Volgende",
+                "previous":   "Vorige"
+            },
+            "aria": {
+                "sortAscending":  ": activeren om kolom oplopend te sorteren",
+                "sortDescending": ": activeren om kolom aflopend te sorteren"
+            }
+        }
+    });
+});
 </script>
