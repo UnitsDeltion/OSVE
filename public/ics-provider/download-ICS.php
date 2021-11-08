e15802e53bb9139dcce3081c7e5936095804e3ce
JesseKoldewijn <jesse@hardwarehulp.nl>
2021-11-08 13:25:03 +0100

    Update download-ICS.php

--- public/ics-provider/download-ICS.php
+++ public/ics-provider/download-ICS.php
@@ -7,10 +7,10 @@ header('Content-Disposition: attachment; filename=Examen.ics');
 
 $ics = new ICS(array(
   'location' => $_SESSION['location'],
-  'description' => "Examen: " . $_SESSION['Examen'] . " Vak: " . $_SESSION['Vak'] . " Opmerkingen: " . $_SESSION['Opmerkingen'],
+  'description' => "Omschrijving",
   'dtstart' => $_SESSION['date_start'],
   'dtend' => $_SESSION['date_end'],
-  'summary' => "Examen ingepland voor student " . $_SESSION['Studentnummer'],
+  'summary' => "Examen ingepland voor student",
   'url' => $_SESSION['url']
 ));
 
