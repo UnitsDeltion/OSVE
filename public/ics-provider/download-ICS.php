100cde61f54bb5d3426d6d56686c22dc74051aab
JesseKoldewijn <jesse@hardwarehulp.nl>
2021-11-08 13:23:55 +0100

    hidden Input-velden verwijderd

--- public/ics-provider/download-ICS.php
+++ public/ics-provider/download-ICS.php
@@ -6,12 +6,12 @@ header('Content-Type: text/calendar; charset=utf-8');
 header('Content-Disposition: attachment; filename=Examen.ics');
 
 $ics = new ICS(array(
-  'location' => $_POST['location'],
-  'description' => $_POST['description'],
-  'dtstart' => $_POST['date_start'],
-  'dtend' => $_POST['date_end'],
-  'summary' => $_POST['summary'],
-  'url' => $_POST['url']
+  'location' => $_SESSION['location'],
+  'description' => "Examen: " . $_SESSION['Examen'] . " Vak: " . $_SESSION['Vak'] . " Opmerkingen: " . $_SESSION['Opmerkingen'],
+  'dtstart' => $_SESSION['date_start'],
+  'dtend' => $_SESSION['date_end'],
+  'summary' => "Examen ingepland voor student " . $_SESSION['Studentnummer'],
+  'url' => $_SESSION['url']
 ));
 
 echo $ics->to_string();
