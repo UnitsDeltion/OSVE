60e9f66abf15e7715a9fc463a1753e2c0c5ca7bc
JesseKoldewijn <jesse@hardwarehulp.nl>
2021-11-11 12:55:50 +0100

    Update p7.blade.php

--- resources/views/p7.blade.php
+++ resources/views/p7.blade.php
@@ -14,6 +14,12 @@
                 <h3 class="align-center">Controleer je e-mail!</h3>
                 <h5 class="align-center">Voordat het examen definitief is ingepland moet deze eerst worden bevestigd. <br> Er is een e-mail verstuurd naar <span class="fc-secondary-nh">{{ $studentnummer }}@st.deltion.nl</span>. Gebruik de link in de e-mail om het examen te bevestigen.</h5>
             </div>
+            <form method="post" action="{{ route('ics_handler') }}">
+                @csrf
+                <x-jet-button class="button" style="float: right">
+                    Download afspraak <i class="fas fa-download ml-2"></i> 
+                </x-jet-button>
+            </form>
         </div>
     </div>
 
