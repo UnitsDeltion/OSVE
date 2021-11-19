94f3e90e7fd0665c414c06e9598b915b9dd055d4
martijnschuman <31829786+martijnschuman@users.noreply.github.com>
2021-11-19 13:34:40 +0100

    Initial push

--- app/Http/Controllers/Beheer/GeplandeExamensBeheerController.php
+++ app/Http/Controllers/Beheer/GeplandeExamensBeheerController.php
@@ -6,21 +6,25 @@ use Illuminate\Http\Request;
 use App\Models\GeplandeExamens;
 use App\Http\Controllers\Controller;
 
-
 class GeplandeExamensBeheerController extends Controller
 {
-    public function bevestigExamen($id){
-
+    public function bevestigExamen($id)
+    {
         //Haalt hte gepland examen record op
-        $geplandExamen = GeplandeExamens::where('id', $id)->first();
+        $geplandExamen = GeplandeExamens::where("id", $id)->first();
 
         //Zet de waarde van doc_bevestigd op 1 zodat hij door de docent is bevestigd
-        $geplandExamen['doc_bevestigd'] = 1;
+        $geplandExamen["doc_bevestigd"] = 1;
 
         //Slaat de record op in de db
         $geplandExamen->save();
 
+        //Bevestigings mail naar student
+        \Mail::to($geplandExamen["studentnummer"] . "@st.deltion.nl")->send(
+            new \App\Mail\examenIngepland()
+        );
+
         //Stuurt terug nar dashboard
-        return redirect('dashboard'); 
-    } 
+        return redirect("dashboard");
+    }
 }
