5091ea6e07af75c2e75615e7311fd1f992dc18ba
martijnschuman <31829786+martijnschuman@users.noreply.github.com>
2021-11-19 15:50:16 +0100

    Code cleanup

--- app/Http/Controllers/Beheer/GeplandeExamensBeheerController.php
+++ app/Http/Controllers/Beheer/GeplandeExamensBeheerController.php
@@ -25,6 +25,6 @@ class GeplandeExamensBeheerController extends Controller
         );
 
         //Stuurt terug nar dashboard
-        return redirect("dashboard");
+        return redirect("/beheer/dashboard");
     }
 }
