15862ff866c09b38549209d7a0243bf9830de063
spatzy <55000624+1spatz1@users.noreply.github.com>
2021-11-24 14:05:02 +0100

    update

--- resources/views/beheer/moments/delete.blade.php
+++ resources/views/beheer/moments/delete.blade.php
@@ -3,11 +3,11 @@
     <div class="modal-body">
         @csrf
         @method('DELETE')
-        <h5 class="text-center">Weet je het zeker dat je het moment van "{{ $moment->tijd }}" op "{{ $moment->datum }}" wilt verwijderen?</h5>
+        <h5 class="text-center">Weet je het zeker dat je het moment van <span class="fc-secondary">"{{ $moment->tijd }}"</span> op <span class="fc-secondary">"{{ $moment->datum }}"</span> wilt verwijderen?</h5>
     </div>
     <div class="modal-footer-custom">
             <div class="mt-4">
-                <a data-bs-dismiss="modal" class="fc-h-white a-clear float-left mb-2 button inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition button float-right">
+                <a data-bs-dismiss="modal" class="pointer fc-h-white a-clear float-left mb-2 button inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition button float-right">
                     <i class="fas fa-backward mr-2" ></i> Cancel
                 </a>
                 <x-jet-button class="button float-right">
