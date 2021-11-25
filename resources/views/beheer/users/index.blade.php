da598b3d5bffba22503c9be0026e35bc54f918c9
martijnschuman <31829786+martijnschuman@users.noreply.github.com>
2021-11-25 15:10:32 +0100

    Docs

--- resources/views/beheer/users/index.blade.php
+++ resources/views/beheer/users/index.blade.php
@@ -15,69 +15,61 @@
 
     @livewire('includes.content.top.content-normal-top') 
 
-        @if($message = Session::get('success'))
-            <div class="alert alert-success alert-dismissible fade show mb-10" role="alert">
-                <p class="mb-0">{{$message}}</p>
-            </div>
-        @endif
+    @if($message = Session::get('success'))
+        <div class="alert alert-success alert-dismissible fade show mb-10" role="alert">
+            <p class="mb-0">{{$message}}</p>
+        </div>
+    @endif
 
-        <table class="table mt-4">
-            <thead>
-                <tr>
-                    <th>Naam</th>
-                    <th>Email</th>
-                    <th colspan="2"></th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($users as $user)
-                    <tr class="va-middle">
-                        <td>{{ $user->voornaam }} {{ $user->achternaam }}</td>
-                        <td>{{ $user->email }}</td>
-                        <td class="align-right pr-0">
-                            <a href="{{ route('users.edit', $user->id) }}" class="mr-2 a-clear">
-                                <x-jet-button class="button">
-                                    <i class="fas fa-edit"></i>
-                                </x-jet-button>
-                            </a>
-                        </td>
-                        <td>
+    <table class="table mt-4">
+        <thead>
+            <tr>
+                <th>Naam</th>
+                <th>Email</th>
+                <th colspan="2"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+                <tr class="va-middle">
+                    <td>{{ $user->voornaam }} {{ $user->achternaam }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td class="align-right pr-0">
+                        <a href="{{ route('users.edit', $user->id) }}" class="mr-2 a-clear">
+                            <x-jet-button class="button">
+                                <i class="fas fa-edit"></i>
+                            </x-jet-button>
+                        </a>
+                    </td>
+                    <td>
                         <a data-toggle="modal" id="largeButton" data-target="#largeModal" data-attr="{{ route('usersDelete', $user->id) }}" title="Delete User">
-                                <!-- <i class="fas fa-trash text-danger  fa-lg"></i> -->
-                                <x-jet-button class="button" title="Verwijderen">
-                                        <i class="fas fa-trash"></i>
-                                </x-jet-button>
-                            </a>
-                            <!-- <form action="{{ route('users.destroy', $user->id) }}" method="POST" onsubmit="return confirm('Weet je het zeker');">
-                                <input type="hidden" name="_method" value="DELETE">
-                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                <x-jet-button class="button">
+                            <x-jet-button class="button" title="Verwijderen">
                                     <i class="fas fa-trash"></i>
-                                </x-jet-button>
-                            </form> -->
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+                            </x-jet-button>
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 
      <!-- large modal -->
-<div class="modal fade" id="largeModal" data-bs-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="largeModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body" id="largeBody">
-                <div>
-                    <!-- body content -->
+    <div class="modal fade" id="largeModal" data-bs-backdrop="static" tabindex="-1" role="dialog" aria-labelledby="largeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body" id="largeBody">
+                    <div>
+                        <!-- body content -->
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 
 <script>
     // display delete modal
