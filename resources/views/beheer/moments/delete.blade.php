c4f8d7d8933f00dbcd3156483e9011738ec2a11c
martijnschuman <31829786+martijnschuman@users.noreply.github.com>
2021-11-25 12:50:25 +0100

    Code fix

--- resources/views/beheer/moments/delete.blade.php
+++ resources/views/beheer/moments/delete.blade.php
@@ -3,12 +3,15 @@
     <div class="modal-body">
         @csrf
         @method('DELETE')
-        <h5 class="text-center">Weet je het zeker dat je het moment van <span class="fc-secondary">"{{ $moment->tijd }}"</span> op <span class="fc-secondary">"{{ $moment->datum }}"</span> wilt verwijderen?</h5>
+        <?php
+            $examenDatumFormatted = date('d-m-Y', strtotime($moment->datum)); 
+        ?>
+        <h5 class="text-center">Weet je het zeker dat je het moment van <span class="fc-secondary">"{{ $moment->tijd }}"</span> op <span class="fc-secondary">"{{ $examenDatumFormatted }}"</span> wilt verwijderen?</h5>
     </div>
     <div class="modal-footer-custom">
             <div class="mt-4">
                 <a data-bs-dismiss="modal" class="pointer fc-h-white a-clear float-left mb-2 button inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition button float-right">
-                    <i class="fas fa-backward mr-2" ></i> Cancel
+                    <i class="fas fa-backward mr-2" ></i> Annuleer
                 </a>
                 <x-jet-button class="button float-right">
                     Ja, verwijder het examen moment <i class="fas fa-forward ml-2"></i> 
