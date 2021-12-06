6f290a632db08508e32e6829765ce0618dbf1523
martijnschuman <martijnschuman@hotmail.com>
2021-12-06 13:35:56 +0100

    Dev/breadcrumbs

--- resources/views/livewire/includes/content/top/content-normal-top.blade.php
+++ resources/views/livewire/includes/content/top/content-normal-top.blade.php
@@ -1,7 +1,15 @@
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-md-10">
-                        <div class="content">
+
+    <?php
+        $route = Route::current();
+        $name = $route->getName();
+    ?>
+
+    @livewire('includes.breadcrumbs', ['page' => $name])
+
+    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-10">
+                    <div class="content">
