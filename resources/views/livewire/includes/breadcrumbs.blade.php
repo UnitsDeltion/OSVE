83c9833f65c9daa314005aa115b9f93bd7fbdbbd
martijnschuman <martijnschuman@hotmail.com>
2021-12-07 00:41:27 +0100

    Actieve examens & docs

--- resources/views/livewire/includes/breadcrumbs.blade.php
+++ resources/views/livewire/includes/breadcrumbs.blade.php
@@ -5,22 +5,22 @@
             $return = '<a href="/" class="a-clear fc-white breadcrumbs-active"><i class="fas fa-home"></i> Home</a>';
         }elseif($page == 'p2'){
             $return = '<a href="/" class="a-clear fc-white"><i class="fas fa-home"></i> Home</a>';
-            $return .= '<i class="fas fa-forward ml-2 mr-2"></i> <a href="/p2" class="a-clear fc-white breadcrumbs-active"><i class="fas fa-graduation-cap"></i> Opleidingen</a>';
+            $return .= '<i class="fas fa-angle-right ml-2 mr-2"></i> <a href="/p2" class="a-clear fc-white breadcrumbs-active"><i class="fas fa-graduation-cap"></i> Opleidingen</a>';
         }elseif($page == 'p3'){
             $return = '<a href="/" class="a-clear fc-white"><i class="fas fa-home"></i> Home</a>';
-            $return .= '<i class="fas fa-forward ml-2 mr-2"></i> <a href="/p2" class="a-clear fc-white"><i class="fas fa-graduation-cap"></i> Opleidingen</a>';
-            $return .= '<i class="fas fa-forward ml-2 mr-2"></i> <a href="/p3" class="a-clear fc-white breadcrumbs-active"><i class="fas fa-user-edit"></i> Examens</a>';
+            $return .= '<i class="fas fa-angle-right ml-2 mr-2"></i> <a href="/p2" class="a-clear fc-white"><i class="fas fa-graduation-cap"></i> Opleidingen</a>';
+            $return .= '<i class="fas fa-angle-right ml-2 mr-2"></i> <a href="/p3" class="a-clear fc-white breadcrumbs-active"><i class="fas fa-user-edit"></i> Examens</a>';
         }elseif($page == 'p4'){
             $return = '<a href="/" class="a-clear fc-white"><i class="fas fa-home"></i> Home</a>';
-            $return .= '<i class="fas fa-forward ml-2 mr-2"></i> <a href="/p2" class="a-clear fc-white"><i class="fas fa-graduation-cap"></i> Opleidingen</a>';
-            $return .= '<i class="fas fa-forward ml-2 mr-2"></i> <a href="/p3" class="a-clear fc-white"><i class="fas fa-user-edit"></i> Examens</a>';
-            $return .= '<i class="fas fa-forward ml-2 mr-2"></i> <a href="/p4" class="a-clear fc-white breadcrumbs-active"><i class="fas fa-clock"></i> Examen moment</a>';
+            $return .= '<i class="fas fa-angle-right ml-2 mr-2"></i> <a href="/p2" class="a-clear fc-white"><i class="fas fa-graduation-cap"></i> Opleidingen</a>';
+            $return .= '<i class="fas fa-angle-right ml-2 mr-2"></i> <a href="/p3" class="a-clear fc-white"><i class="fas fa-user-edit"></i> Examens</a>';
+            $return .= '<i class="fas fa-angle-right ml-2 mr-2"></i> <a href="/p4" class="a-clear fc-white breadcrumbs-active"><i class="fas fa-clock"></i> Examen moment</a>';
         }elseif($page == 'p5'){
             $return = '<a href="/" class="a-clear fc-white"><i class="fas fa-home"></i> Home</a>';
-            $return .= '<i class="fas fa-forward ml-2 mr-2"></i> <a href="/p2" class="a-clear fc-white"><i class="fas fa-graduation-cap"></i> Opleidingen</a>';
-            $return .= '<i class="fas fa-forward ml-2 mr-2"></i> <a href="/p3" class="a-clear fc-white"><i class="fas fa-user-edit"></i> Examens</a>';
-            $return .= '<i class="fas fa-forward ml-2 mr-2"></i> <a href="/p4" class="a-clear fc-white"><i class="fas fa-clock"></i> Examen moment</a>';
-            $return .= '<i class="fas fa-forward ml-2 mr-2"></i> <a href="/p5" class="a-clear fc-white breadcrumbs-active"><i class="fas fa-eye"></i> P5</a>';
+            $return .= '<i class="fas fa-angle-right ml-2 mr-2"></i> <a href="/p2" class="a-clear fc-white"><i class="fas fa-graduation-cap"></i> Opleidingen</a>';
+            $return .= '<i class="fas fa-angle-right ml-2 mr-2"></i> <a href="/p3" class="a-clear fc-white"><i class="fas fa-user-edit"></i> Examens</a>';
+            $return .= '<i class="fas fa-angle-right ml-2 mr-2"></i> <a href="/p4" class="a-clear fc-white"><i class="fas fa-clock"></i> Examen moment</a>';
+            $return .= '<i class="fas fa-angle-right ml-2 mr-2"></i> <a href="/p5" class="a-clear fc-white breadcrumbs-active"><i class="fas fa-eye"></i> Overzicht</a>';
         }else{
             $return = null;
         }
